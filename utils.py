a8a7218cab15ef598f5a1629c1a4225124f3428f
BrianOrea00 <deathscytheiv@gmail.com>
2026-04-14 22:26:36 +0800

    UTILS

---
+++ utils.py
@@ -0,0 +1,17 @@
+import json
+import os
+import config import HISTORY_FILE
+
+def save_history(data):
+    if not os.path.exists(HISTORY_FILE):
+        with open(HISTORY_FILE, "w") as f:
+            json.dump([], f)
+
+    with open(HISTORY_FILE, "r+") as f:
+        history = json.load(f)
+        history.append(data)
+        f.seek(0)
+        json.dump(history, f, indent=4)
+
+def clean_filename(name):
+    return "".join(c for c in name if c not in r'\/:*?"<>|')
